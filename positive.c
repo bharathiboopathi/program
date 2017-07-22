f5b70951fb758b410017647361d2ec14ed42e7bc
bharathiboopathi <30343137+bharathiboopathi@users.noreply.github.com>
2017-07-22 10:55:22 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the num:");
+    scanf("%d",&n);
+    if(n!=0)
+        if(n>0)
+        {
+            printf("the pasitive");
+        }
+        else
+        printf("the negative");
+    else
+        printf("zero");
+        return 0;
+}
